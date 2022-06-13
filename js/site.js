31a69c36e64718c9ac9fce62bb2a840575239b88
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2022-06-13 15:14:14 -0400

    Completed the Code page to add all descriptions

--- js/site.js
+++ js/site.js
@@ -29,23 +29,10 @@ function getValues() {
 
 }
 
-// function monthPayment(loanAmount, months, rate){
-    
-//     let monthlyPayment = 0;
-    
-//     monthlyPayment = (loanAmount) * (rate/1200) / (1 - Math.pow((1 + rate/1200), -months));
-
-//     monthlyPayment = Math.round((monthlyPayment + Number.EPSILON) * 100) / 100;
-
-//     return monthlyPayment;
-
-// }
-
 //Generate amortization schedule for length of loan
 //Logic function(s)
 function loanCalc(loanAmount, months, rate) {
     //calculate monthly payment
-    //let monthlyPayment = 0;
     let monthlyPayment = (loanAmount) * (rate/1200) / (1 - Math.pow((1 + rate/1200), -months));
     monthlyPayment = Math.round((monthlyPayment + Number.EPSILON) * 100) / 100;
 
@@ -53,10 +40,8 @@ function loanCalc(loanAmount, months, rate) {
     let loanPayments = [];
     let month = 1;
     let interest = Math.round(((loanAmount * (rate/1200)) + Number.EPSILON) * 100) / 100;
-    //let interest = loanAmount * (rate/1200);
     let totInterest = interest;
     let principal = Math.round(((monthlyPayment - interest) + Number.EPSILON) * 100) / 100;
-    //let principal = monthlyPayment - interest;
     let balance = Math.round(((loanAmount - principal) + Number.EPSILON) * 100) / 100;
     let loanPayment = {};
 
@@ -77,12 +62,9 @@ function loanCalc(loanAmount, months, rate) {
         if( month < months){
         month += 1;
         interest = Math.round(((balance * (rate/1200)) + Number.EPSILON) * 100) / 100;
-        //interest = balance * (rate/1200);
         totInterest = Math.round(((totInterest + interest) + Number.EPSILON) * 100) / 100;
         principal = Math.round(((monthlyPayment - interest) + Number.EPSILON) * 100) / 100;
-        //principal = monthlyPayment - interest;
         balance = Math.round(((balance - principal) + Number.EPSILON) * 100) / 100;
-        //balance = balance - principal;
         }
         
     }
@@ -95,9 +77,6 @@ function loanCalc(loanAmount, months, rate) {
     };
 }
 
-
-
-//Display numbers and set Classes for 'Fizz', 'Buzz' or 'FizzBuzz'
 //Display or view functions
 function displayLoan(loanData) {  
 
@@ -115,16 +94,12 @@ function displayLoan(loanData) {
 
 
     let tableBody = document.getElementById("results");
-    //Get template element
-    //let templateRow = document.getElementById("lsTemplate");
     //clear table first to ensure correct data is displayed
     tableBody.innerHTML = "";
 
     for (let index = 0; index < paymentSchedule.length; index ++) {
 
             let tableRow = document.createElement('tr');
-            //grab just <td> and put them into an array to count them
-            //let rowCols = tableRow.querySelectorAll('td');
 
             let rowData1 = document.createElement('td');
             rowData1.innerHTML = paymentSchedule[index].month;
@@ -145,21 +120,6 @@ function displayLoan(loanData) {
             tableRow.appendChild(rowData4);
             tableRow.appendChild(rowData5);
             tableRow.appendChild(rowData6);
-
-            //rowCols[0].classList.add(fizzArray[index]);
-            // rowCols[0].textContent = loanData[index];
-
-            // rowCols[1].classList.add(fizzArray[index + 1]);
-            // rowCols[1].textContent = fizzArray[index + 1];
-
-            // rowCols[2].classList.add(fizzArray[index + 2]);
-            // rowCols[2].textContent = fizzArray[index + 2];
-
-            // rowCols[3].classList.add(fizzArray[index + 3]);
-            // rowCols[3].textContent = fizzArray[index + 3];
-
-            // rowCols[4].classList.add(fizzArray[index + 4]);
-            // rowCols[4].textContent = fizzArray[index + 4];
 
             tableBody.appendChild(tableRow);
         }
